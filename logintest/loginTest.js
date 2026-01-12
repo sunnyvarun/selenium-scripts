ee44a5d599604a38dc902ff81cbda856b1bf58ba
Varunkanth <varunsunchu59@gmail.com>
2026-01-12 14:37:33 +0530

    Update loginTest.js

--- logintest/loginTest.js
+++ logintest/loginTest.js
@@ -2,7 +2,7 @@ const { Builder, By, until } = require('selenium-webdriver');
 
 async function runTest (){
           let driver = await new Builder ().forBrowser('chrome').build();
-          await driver.get('https://eduportal.webhostdevs.com/login.php');
+          await driver.get('your-website-url');
           await driver.findElement(By.css('[type="text"]')).sendKeys('your-username');
           await driver.findElement(By.xpath("//input[@name='password']")).sendKeys('your-password');
           await driver.findElement(By.id('submit')).click();
