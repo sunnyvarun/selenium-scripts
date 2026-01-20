7b88c187a1cecdacdb6af9df1c57d864ac67d5ca
varun sunny <varunsunchu59@gmail.com>
2026-01-20 23:56:11 +0530

    passwords removed

--- verificationtest/urlverification.js
+++ verificationtest/urlverification.js
@@ -2,11 +2,11 @@ const { Builder, By, until } = require('selenium-webdriver');
 
 async function runTest (){
           let driver = await new Builder ().forBrowser('chrome').build();
-          await driver.get('https://eduportal.webhostdevs.com/login.php');
+          await driver.get('');
           await driver.findElement(By.id('admin-card')).click();
           await driver.findElement(By.id('username')).clear();
-          await driver.findElement(By.id('username')).sendKeys('sunny');
-          await driver.findElement(By.xpath("//input[@name='password']")).sendKeys('sunny37');
+          await driver.findElement(By.id('username')).sendKeys('');
+          await driver.findElement(By.xpath("//input[@name='password']")).sendKeys('');
           await driver.sleep(500);
           await driver.findElement(By.id('submit')).click();
           await driver.wait(until.elementLocated(By.className('alert')),10000);
--- verificationtest/verificationTest.js
+++ verificationtest/verificationTest.js
@@ -4,10 +4,10 @@ const { expect } = require('chai');
 async function testDashboard() {
   let driver = await new Builder().forBrowser('chrome').build();
 
-  await driver.get('https://eduportal.webhostdevs.com/login.php');
+  await driver.get('');
 
-  await driver.findElement(By.css('[type="text"]')).sendKeys('Varun');
-          await driver.findElement(By.xpath("//input[@name='password']")).sendKeys('varun37');
+  await driver.findElement(By.css('[type="text"]')).sendKeys('');
+          await driver.findElement(By.xpath("//input[@name='password']")).sendKeys('');
   await driver.findElement(By.id('submit')).click();
 
   await driver.wait(until.elementLocated(By.id('title')), 10000);
