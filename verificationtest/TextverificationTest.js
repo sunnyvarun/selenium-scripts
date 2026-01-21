e53d5c53697334907944a734051819b567d28d42
varun sunny <varunsunchu59@gmail.com>
2026-01-21 23:49:56 +0530

    Dashboard verification through Text Matching

--- verificationtest/TextverificationTest.js
+++ verificationtest/TextverificationTest.js
@@ -4,17 +4,17 @@ const { expect } = require('chai');
 async function testDashboard() {
   let driver = await new Builder().forBrowser('chrome').build();
 
-  await driver.get('');
+  await driver.get('https://eduportal.webhostdevs.com/login.php');
 await driver.wait(
-          until.elementLocated(By.id('title')),
+          until.elementLocated(By.tagName('h2')),
           10000
         );
         
         const titleText = await driver
-          .findElement(By.id('title'))
+          .findElement(By.tagName('h2'))
           .getText();
         
-        if (titleText === 'Dashboard') {
+        if (titleText === 'EduPortal') {
           console.log('TEST PASSED');
         } else {
           console.log('TEST FAILED');
